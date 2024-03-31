5394f4ed374c54569f40d570692d9c5438ffc097
ayushdama11 <ayushdama07@gmail.com>
2024-03-31 20:00:47 +0530

    learned events in node js

--- eventsNode.js
+++ eventsNode.js
@@ -1,6 +1,21 @@
-// events is built in module - create, fire, listen for events
+// "events" is built in module - create, fire, listen for events
 
 //example 1= registering for the event to be fired only one time using once
+
 //example 2= create an event emitter instance and register a couple of callbacks
+
 //example 3= registering for event with callback parameters
 
+const EventEmitter=require('events');
+const event= new EventEmitter();
+
+event.on("sayMyName", ()=>{
+    console.log("your name is ayush");
+})
+
+event.on("checkPage", (sc,msg)=>{
+    console.log(`status code is ${sc} and the page is ${msg}`);
+})
+
+event.emit("checkPage", 200, "ok");
+event.emit("sayMyName");

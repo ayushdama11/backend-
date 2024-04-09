b6ec2c5a7aa2db00e07406c80d4d7e7f9e53b21a
ayushdama11 <ayushdama07@gmail.com>
2024-04-09 09:05:39 +0530

    updation

--- yt/express1.js
+++ yt/express1.js
@@ -6,35 +6,72 @@ const products= data.products;
 const express= require("express");
 const server=express();
 
-//middleware - server.use()
+// ** third party middlewares like cookies
+// *morgan is a built in third party middleware logger in express
+const morgan=require('morgan');
+// server.use(morgan('dev'));  //GET /data.json 200 108.154 ms - 22114
+server.use(morgan('default'));
+
+//*** middleware - server.use() likhna hota hai to make a middleware
 //this is a type of logger middleware, server pe jo b request jaati hai uska log bana skte haii
-server.use((req,res,next)=>{
-    console.log(req.get('User-Agent'),req.method,new Date(), req.ip, req.hostname)   //GET ::1 localhost -- ::1 is shortform of localhost
-    next(); //batata hai ki aap aage ja sakte ho
-})
+
+//***this is application level middleware 
+// server.use((req,res,next)=>{
+//     console.log(req.get('User-Agent'),req.method,new Date(), req.ip, req.hostname)   //GET ::1 localhost -- ::1 is shortform of localhost
+//     next(); //batata hai ki aap aage ja sakte ho
+// })
+
+//GET ::1 localhost -- ::1 is shortform of localhost
+
+
+// ** built in middlewares - 
+//bodyParser - iske bina body kaam nai karegii
+server.use(express.json()); //now it can read json which is there in the body in the postman 
+// server.use(express.urlencoded()); //used mainly in form where there are url's
+
+// server.static built in middleware
+//iska matlab public naam ke folder me mene ek file banaya hai waha se koi bhi file uthaoo aur host kardo
+server.use(express.static('public'));   //so basically this static folder is a type of remote folder which we can access using only /
+
 
 // middleware name auth
-const auth=(req,res,next)=>{
-    console.log(req.method)   //GET ::1 localhost -- ::1 is shortform of localhost
-    next(); //batata hai ki aap aage ja sakte ho
-}
+//*** router level middleware
+// http://localhost:8080/?password=123 :- here ?password is a req.query 
 
+const auth=(req,res,next)=>{
+    // console.log(req.query)   
+
+    // if(req.query.password=='123'){
+    if(req.body.password=='123'){
+        next()
+    }else{
+        res.sendStatus(401);
+    }
+    // next(); //batata hai ki aap aage ja sakte ho
+}
+// server.use(auth);
 
 
 //*creating api's - returning json
+//CRUD - POST, GET, PUT, DELETE
 //API -Endpoint - Route
-server.get('/', (req,res)=>{
+//get is used to read something 
+server.get('/',auth,(req,res)=>{
     res.json({type: 'GET'})
 })
-server.post('/', (req,res)=>{
+// post is used to post or we can say give some response
+server.post('/',auth,(req,res)=>{
     res.json({type: 'POST'})
 })
+// put is used to replace something or we can say that it is used to update something
 server.put('/', (req,res)=>{
     res.json({type: 'PUT'})
 })
+//delete
 server.delete('/', (req,res)=>{
     res.json({type: 'DELETE'})
 })
+//patch is used to partially modify something not completely
 server.patch('/', (req,res)=>{
     res.json({type: 'PATCH'})
 })

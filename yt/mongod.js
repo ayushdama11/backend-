01a144fae9970199808f44b536be63090462518b
ayushdama11 <ayushdama07@gmail.com>
2024-04-20 19:43:27 +0530

    learned mongodb and mongodb atlas

---
+++ yt/mongod.js
@@ -0,0 +1,5 @@
+// **nosql database structure
+// db server - databse - collections - document
+// example dbserver - university - student , courses, teachers ke collection - records [documents]
+// har document me json data hota haii
+
